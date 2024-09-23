a8612ca0cc962e70ad193c519d1ad1531ba23712
gvqsilva <gvqsilva@gmail.com>
2024-09-23 15:17:42 -0300

    Footer feito; não consigo arrumar o Header/Nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import { FooterGeral } from "./styledFooter.js"
+
 const Footer = () => {
     return(
-        <>
+        <FooterGeral>
             <h5>&copy; 2024 G4Tech. Todos os direitos reservados.</h5>
-        </>
+        </FooterGeral>
     )
 }
 export default Footer
---
+++ src/components/styledFooter.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const FooterGeral = styled.footer`
+
+  background-color: var(--black-color-light);
+  color: white;
+  text-align: center;
+  padding: 1rem;
+  bottom: 0;
+  width: 100%;
+
+`
