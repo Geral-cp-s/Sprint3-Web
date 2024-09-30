9b9d7fea2e85454f4a87831f20d469a6b20ca8c2
gvqsilva <gvqsilva@gmail.com>
2024-09-30 19:22:04 -0300

    Add img do github que direciona para o git do projeto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import { FooterGeral } from "./styledFooter.js"
+import { FaGithub } from "react-icons/fa";
+
 
 const Footer = () => {
     return(
         <FooterGeral>
             <h5>&copy; 2024 G4Tech. Todos os direitos reservados.</h5>
+            <a href="https://github.com/Geral-cp-s/Sprint3-Web" target="_blank"><FaGithub />
+            </a>
+            
+
         </FooterGeral>
     )
 }
