298d2c40e86b4e1b490789a2dd8e6c76ec801f7b
Augusto <auguston22@gmail.com>
2024-09-30 08:35:36 -0300

    Home (videoe materias)

--- src/components/Home/Sec3.jsx
+++ src/components/Home/Sec3.jsx
@@ -4,7 +4,21 @@ export default function Sec3(){
 
     return(
         <SectionGeral className="secao3">
-            <h2>Matéria 3</h2>
+            <h2>F-E: Cofundador promete evento "maior e melhor" em São Paulo</h2>
+            <h3>Alberto Longo, cofundador da Fórmula E, acredita que EP de São Paulo será ainda melhor que 2023-24; Gen3 EVO correrá pela primeira vez no Brasil</h3>
+            <p>São Paulo será a grande estreia da temporada 2024-25 da Fórmula E.  Alberto Longo, cofundador da categoria de monopostos elétricos, afirmou, em coletiva de imprensa, que evento será "maior e melhor". O EP inaugural acontecerá no dia 7 de dezembro deste ano, no sambódromo do Anhembi.
+
+Longo elogiou o EP deste ano, que aconteceu em março, citando como a capital paulista acolhe a categoria e é apaixonada pelo automobilismo: "Vocês nos fazem sentir em casa". "Não sou só eu que falo isso, como cofundador da F-E, mas falo em nome das escuderias, dos pilotos e das pessoas que assistem e seguem essa paixão".
+
+Lucas Di Grassi, que irá correr em nova equipe em 2024-25, com a Yamaha-Lola, parceria de fábrica e estreante na categoria, elogiou os novos carros, citando os "mil quilômetros de teste" já realizados com o monoposto da fabricante japonesa.
+
+Como explicado pelo piloto brasileiro, a etapa será o ponto de partida para a nova versão dos carros, o Gen3 EVO, que promete ter aceleração de 0 à 100km/h em apenas 1,8 segundos, contra os 2,8s anteriores, além de tração nas quatro rodas quando os carros entrarem em "modo de ataque", permitindo mais ultrapassagens, mesmo em pistas como em Mônaco.
+
+Outras mudanças incluem novos pneus, com maior porcentagem de compostos reciclados, e aerodinâmica remodelada. Longo enfatizou a evolução tecnológica da categoria como objetivo e como pensa ter participado no processo de avanço dos carros elétricos no mundo.
+
+Além disso, citou como outros focos o crescimento da categoria, que chamou de "bebê", se comparada com outras categorias históricas; a liderança em esportes sustentáveis e a visita à cidades "incríveis" pelo mundo.
+
+Além disso, estava presente na mesa da coletiva o prefeito de São Paulo, Ricardo Nunes. O EP de São Paulo acontece desde a temporada 2022-23, com vitórias de Mitch Evans e Sam Bird, mas com mudanças no calendário a cada ano.</p>
         </SectionGeral>
     )
 }
